da778819d1f5ea06a2d8f3c9ffc3d6f5e3591670
sumeshsharma00 <130652891+sumeshsharma00@users.noreply.github.com>
2023-07-11 13:37:53 +0530

    Update JDBCDataSource.java

--- ORS_Project4/src/main/java/com/rays/pro4/util/JDBCDataSource.java
+++ ORS_Project4/src/main/java/com/rays/pro4/util/JDBCDataSource.java
@@ -14,7 +14,7 @@ import com.rays.pro4.exception.ApplicationException;
  * @author Sumesh Sharma
  *
  */
-public class JDBCDataSource {
+public final class JDBCDataSource {
 
 	 /**
      * JDBC Database connection pool ( DCP )
